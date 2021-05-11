9cad22af5063784f57aec58e56e3f9087a009b9c
Toba <adesanyatoba@gmail.com>
2021-05-11 08:52:20 +0100

    add_wo_plus.c

--- Leetcode Quizes/add_wo_plus.c
+++ Leetcode Quizes/add_wo_plus.c
@@ -1,3 +1,7 @@
+/*  Program to add two numbers without using the 
+*   +, -, * or / operators.
+*/
+
 #include <stdio.h>
 
 long binary_conv(int dec_num)
@@ -14,30 +18,36 @@ long binary_conv(int dec_num)
     return bin_num;
 }
 
+int Add(int x, int y)
+{
+    // iterate to find all the carry
+    while (y != 0){
+        // carry will contain the common bits of x and y
+        int carry = x & y;
+
+        // sum of bits of x and y where at least one of the bits is not set
+        x = x ^ y;
+
+        // Carry is shifted by one so that adding it to x gives the required sum
+        y = carry << 1;
+    }
+    return x;
+}
+
 int main()
 {
-    // int dec_num;
-    // long bin_num = 0l;
-    // printf("Enter the number to convert: ");
-    // scanf("%d", &dec_num);
-    // bin_num = binary_conv(dec_num);
-    // printf("The binary of %d is %ld\n", dec_num, bin_num);
-
     // Add to the number without using +
     // try 2 + 2 first
     // long result = 2 << 1; // this equals 4
 
-    int num1, num2;
+    int num1, num2, result;
     printf("Enter the first number: ");
     scanf("%d", &num1);
     printf("Enter the second number: ");
     scanf("%d", &num2);
 
-    long result;
-    if ((num1 || num2) > 15){
-        num1 = num1 >> 4;
-    }
-    result = num1 | num2;
+    result = Add(num1, num2);
+
     printf("Result in binary: %ld\n", binary_conv(result));
     printf("Result in decimal: %d\n", result);
 }
