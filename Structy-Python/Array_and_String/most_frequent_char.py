4022eb59630ce1910f32f433ed269d34d88b235e
Toba-Adesanya <adesanyatoba@gmail.com>
2025-03-08 20:42:02 +0000

    most_frequent_char

---
+++ Structy-Python/Array_and_String/most_frequent_char.py
@@ -0,0 +1,38 @@
+"""
+Time:
+Space:
+
+Write a function, most_frequent_char, that takes in
+a string as an argument. The function should return
+the most frequent character of the string. If there
+are ties, return the character that appears earlier
+in the string.
+
+You can assume that the input string is non-empty.
+"""
+
+
+def most_frequent_char(s):
+
+    str_dict: dict = {}
+
+    # Count chars in the string
+    for char in s:
+        if char not in str_dict:
+            str_dict[char] = 1
+        else:
+            str_dict[char] += 1
+
+    # I want to return the char with the largest value
+    print(str_dict)
+
+    max_char = None
+    for char in s:
+        if max_char is None or str_dict[char] > str_dict[max_char]:
+            max_char = char
+
+    print(max_char)
+    return max_char
+
+
+most_frequent_char("helloo")
