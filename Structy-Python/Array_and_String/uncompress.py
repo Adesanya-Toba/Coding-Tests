6f78995eaf88b9f47b7bf05a0d14f3607cc4ebe8
Toba-Adesanya <adesanyatoba@gmail.com>
2025-03-15 21:03:39 +0000

    longest word

---
+++ Structy-Python/Array_and_String/uncompress.py
@@ -0,0 +1,18 @@
+"""
+Time: O()
+Space: O()
+
+Write a function, uncompress, that takes in a string as an argument.
+The input string will be formatted into multiple groups according
+to the following pattern:
+
+<number><char>
+
+for example, '2c' or '3a'.
+The function should return an uncompressed version of the string
+where each 'char' of a group is repeated 'number' times consecutively.
+"""
+
+
+def uncompress(s):
+    pass  # todo
---
+++ Structy-Python/Intro/longest_word.py
@@ -0,0 +1,43 @@
+"""15/03/2025
+Time:
+Space:
+
+Write a function, longest_word, that takes in a sentence
+string as an argument. The function should return the longest
+word in the sentence. If there is a tie, return the word
+that occurs later in the sentence.
+
+You can assume that the sentence is non-empty.
+"""
+
+
+def longest_word(sentence: str):
+    # I would use the split method to split on whitespace to identify words
+    # I might use a dict to keep records of longest words
+    # I also need a variable to keep max length? Maybe
+
+    sentence_dict: dict = {}
+    max_length: int = 0
+
+    for word in sentence.split():
+        word_len = len(word)
+        sentence_dict[word_len] = word
+        if max_length < word_len:
+            max_length = word_len
+
+    print(sentence_dict)
+    print(f"Max word length: {sentence_dict[max_length]}")
+    return sentence_dict[max_length]
+
+
+def longest_word_opt(sentence: str):
+    longest = ""
+
+    for word in sentence.split():
+        if len(word) >= len(longest):
+            longest = word
+    print(longest)
+    return longest
+
+
+longest_word_opt("the quick brown fox jumped over the lazy dog")
