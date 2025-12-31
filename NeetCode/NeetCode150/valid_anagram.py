b8d0e884051dfa5b1c1dd97803ff30ec29296b96
Toba-Adesanya <adesanyatoba@gmail.com>
2025-12-31 02:17:48 +0000

    Alternate solution to anagram

--- NeetCode/NeetCode150/valid_anagram.py
+++ NeetCode/NeetCode150/valid_anagram.py
@@ -24,7 +24,33 @@ class Solution:
 
         return char_store
 
+    # Time: O(n + m), Space: O(n); where n and m are the lengths of s and t.
+
+    def isAnagramNoDict(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+        # Solving with just arrays instead
+        char_freq = [0] * 26  # array of 26 zeros to represent the alphabet
+
+        # Iterate through both strings, incrementing seen chars for one while
+        # decrementing for the other
+
+        for i in range(len(s)):
+            # ******** ord() assumes all words are in lowercase *********
+            char_freq[ord(s[i]) - ord("a")] += 1  # Increment alphabet index when seen
+            char_freq[ord(t[i]) - ord("a")] -= 1  # Decrement alphabet index when seen
+
+        # char_freq should be all zeros if the words are anagrams
+        return (
+            len(set(char_freq)) == 1
+        )  # This is because converting to a set will remove duplicates
+        # We can also use a simple for loop.
+
+    # Time: O(n), Space: O(1) because we use at most 26 characters.
+
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.isAnagram("racecar", "caRrace"))
+    print(sol.isAnagram("racecar", "carrace"))
+    print(sol.isAnagramNoDict("racecar", "carrace"))
