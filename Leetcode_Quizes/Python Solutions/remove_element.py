de71aa103e37489bcdd741282369f8b6119ec6c4
Toba-Adesanya <adesanyatoba@gmail.com>
2026-01-10 23:49:15 +0000

    initial remove element solution

---
+++ Leetcode_Quizes/Python Solutions/remove_element.py
@@ -0,0 +1,48 @@
+"""Given an integer array nums and an integer val, remove all 
+occurrences of val in nums in-place. The order of the elements may be changed. 
+Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, 
+to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain 
+the elements which are not equal to val. The remaining elements of 
+nums are not important as well as the size of nums.
+Return k.
+"""
+
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i:int = 0
+        j:int = len(nums) - 1 
+
+        while i <= j:
+            if nums[i] != val:
+                i += 1
+            else:
+                if nums[j] != val:
+                    temp = nums[i]
+                    nums[i] = nums[j]
+                    nums[j] = temp
+                else:
+                    j -= 1
+
+        print(nums)
+        print(j + 1)
+        return j + 1
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.removeElement([3,2,2,3], 3) # 2
+    sol.removeElement([0,1,2,2,3,0,4,2], 2) # Output: 5, nums = [0,1,4,0,3,_,_,_]
+    sol.removeElement([2], 3) # [2]
+    #                  i             j
+    #                  0 1 2 3 4 5 6 7
+    #                 [0,1,4,0,3,2,2,2]
+
+    # i = 0, j = 7  
+    # [2,2,3,3]
